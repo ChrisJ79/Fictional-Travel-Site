4bd7c1196dbe0abc43146a20db5423a8eb89a610
Chris <csjohnson79@icloud.com>
2019-11-11 20:04:07 -0700

    Made several attempts to get Browsersync to launch from Gulpfile.js, but to no avail.  Stopping Point.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var autoprefixer = require('autoprefixer');
 var cssVars = require('postcss-simple-vars');
 var cssNested = require('postcss-nested');
 var cssImport = require('postcss-import');
-var browsersync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
 
 
 gulp.task('default', function() {
@@ -24,14 +24,41 @@ gulp.task('styles', function() {
 });
 
 
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    // proxy: 'localhost:3000',
+    // port: 8080,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
 
 gulp.task('watch', function() {
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
+
 
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('styles');
 	});
+});
+
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
 
